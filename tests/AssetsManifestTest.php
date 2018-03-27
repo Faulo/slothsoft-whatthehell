47d191ddd1a3cca027fd8618391561133eeec01a
Faulo <FauloLio@gmail.com>
2018-03-27 23:37:52 +0200

    ModuleTests

--- tests/AssetsManifestTest.php
+++ tests/AssetsManifestTest.php
@@ -1,11 +1,11 @@
 <?php
 namespace tests;
 
-use Slothsoft\Farah\ModuleTests\XmlManifestTest;
+use Slothsoft\Farah\ModuleTests\AbstractXmlManifestTest;
 use Slothsoft\Farah\Module\Manifest\ManifestInterface;
 use Slothsoft\Farah\Module\Manifest\XmlManifest;
 
-class AssetsManifestTest extends XmlManifestTest
+class AssetsManifestTest extends AbstractXmlManifestTest
 {
     protected static function loadManifest() : ManifestInterface {
         return new XmlManifest(dirname(__DIR__) . DIRECTORY_SEPARATOR . 'assets' . DIRECTORY_SEPARATOR . '.xml');
--- tests/AssetsModuleTest.php
+++ tests/AssetsModuleTest.php
@@ -2,11 +2,11 @@
 namespace tests;
 
 use Slothsoft\Farah\Module\Module;
-use Slothsoft\Farah\ModuleTests\ModuleTest;
+use Slothsoft\Farah\ModuleTests\AbstractModuleTest;
 use Slothsoft\Farah\Module\FarahUrl\FarahUrlAuthority;
 use Slothsoft\Farah\Module\Manifest\XmlManifest;
 
-class AssetsModuleTest extends ModuleTest
+class AssetsModuleTest extends AbstractModuleTest
 {
     protected static function loadModule() : Module {
         return new Module(
