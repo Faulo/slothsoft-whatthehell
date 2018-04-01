2e25f2b06fa264cf0dbe5a8c84c5a6dc71a5d33e
Faulo <FauloLio@gmail.com>
2018-04-01 17:38:26 +0200

    formatting

--- src/Slothsoft/WhatTheHell/Assets/Push.php
+++ src/Slothsoft/WhatTheHell/Assets/Push.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types = 1);
 namespace Slothsoft\WhatTheHell\Assets;
 
 use Slothsoft\Farah\Module\Node\Asset\AssetImplementation;
@@ -6,31 +7,32 @@ use Slothsoft\Farah\Module\Node\Asset\AssetImplementation;
 class Push extends AssetImplementation
 {
     /*
-     if (! isset($_REQUEST['type'])) {
-     $_REQUEST['type'] = 'message';
-     }
-     if (! isset($_REQUEST['handle'])) {
-     header('HTTP/1.1 400 Bad Request');
-     die('400 Bad Request');
-     }
-     
-     $handle = $_REQUEST['handle'];
-     
-     $insert = array(
-     'time' => $_SERVER['REQUEST_TIME_FLOAT'],
-     'data' => file_get_contents('php://input'),
-     'type' => $_REQUEST['type']
-     );
-     // $insert['data'] = 'Hallo Welt ' . $_SERVER['REQUEST_TIME'];
-     
-     if (strlen($insert['data'])) {
-     DB::setDatabase('whatthehell');
-     DB::insert($handle, $insert);
-     }
-     
-     header("HTTP/1.1 204 No Content");
-     die();
-     
-     //*/
+     * if (! isset($_REQUEST['type'])) {
+     * $_REQUEST['type'] = 'message';
+     * }
+     * if (! isset($_REQUEST['handle'])) {
+     * header('HTTP/1.1 400 Bad Request');
+     * die('400 Bad Request');
+     * }
+     *
+     * $handle = $_REQUEST['handle'];
+     *
+     * $insert = array(
+     * 'time' => $_SERVER['REQUEST_TIME_FLOAT'],
+     * 'data' => file_get_contents('php://input'),
+     * 'type' => $_REQUEST['type']
+     * );
+     * // $insert['data'] = 'Hallo Welt ' . $_SERVER['REQUEST_TIME'];
+     *
+     * if (strlen($insert['data'])) {
+     * DB::setDatabase('whatthehell');
+     * DB::insert($handle, $insert);
+     * }
+     *
+     * header("HTTP/1.1 204 No Content");
+     * die();
+     *
+     * //
+     */
 }
 
--- src/registerModule.php
+++ src/registerModule.php
@@ -3,6 +3,4 @@ use Slothsoft\Farah\Module\Module;
 use Slothsoft\Farah\Module\ModuleRepository;
 use Slothsoft\Farah\Module\FarahUrl\FarahUrlAuthority;
 
-ModuleRepository::getInstance()->registerModule(
-    new Module(FarahUrlAuthority::createFromVendorAndModule('slothsoft', 'whatthehell'),
-        dirname(__DIR__) . DIRECTORY_SEPARATOR . 'assets'));
+ModuleRepository::getInstance()->registerModule(new Module(FarahUrlAuthority::createFromVendorAndModule('slothsoft', 'whatthehell'), dirname(__DIR__) . DIRECTORY_SEPARATOR . 'assets'));
