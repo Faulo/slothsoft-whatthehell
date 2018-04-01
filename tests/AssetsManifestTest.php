387a851dec9f588198042bd4aae900d587df8d72
Faulo <FauloLio@gmail.com>
2018-04-02 00:21:24 +0200

    assets/.xml => assets/manifest.xml

--- tests/AssetsManifestTest.php
+++ tests/AssetsManifestTest.php
@@ -4,10 +4,11 @@ namespace tests;
 use Slothsoft\Farah\ModuleTests\AbstractXmlManifestTest;
 use Slothsoft\Farah\Module\Manifest\ManifestInterface;
 use Slothsoft\Farah\Module\Manifest\XmlManifest;
+use Slothsoft\Farah\Module\Module;
 
 class AssetsManifestTest extends AbstractXmlManifestTest
 {
     protected static function loadManifest() : ManifestInterface {
-        return new XmlManifest(dirname(__DIR__) . DIRECTORY_SEPARATOR . 'assets' . DIRECTORY_SEPARATOR . '.xml');
+        return new XmlManifest(dirname(__DIR__) . DIRECTORY_SEPARATOR . 'assets' . DIRECTORY_SEPARATOR . Module::FILE_MANIFEST);
     }
 }
